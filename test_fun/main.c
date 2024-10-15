fd5509ed032ecf763eaf46846e981b1ae4eefa54
Joao Francisco Perpectuo Figueiredo <jperpect@c2r5s1.42porto.com>
2024-10-15 13:38:49 +0100

    start_the_terminal_line

---
+++ test_fun/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int main(void)
+{
+	char *line;
+	line =readline("oi: ");
+	add_history(line);
+	line =readline("oi: ");
+
+	printf("%s\n",line);
+
+
+}
