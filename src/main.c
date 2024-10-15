e31b8e45e5f334be8107441c0a6f40a9e5e5c5bd
Joao Francisco Perpectuo Figueiredo <jperpect@c2r5s1.42porto.com>
2024-10-15 13:52:06 +0100

    read_line start

--- src/main.c
+++ src/main.c
@@ -1,4 +1,3 @@
-/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
@@ -8,6 +7,7 @@
 /*   Created: 2024/09/10 12:20:37 by jperpect          #+#    #+#             */
 /*   Updated: 2024/09/10 13:12:18 by jperpect         ###   ########.fr       */
 /*                                                                            */
+/* ************************************************************************** */
 /* ************************************************************************** */
 
 #include "minishell.h"
@@ -20,28 +20,10 @@ void func_test ()
 
 int main()
 {
-	ft_printf("ola",0);
-	ft_printf("adeus",0);
-	ft_printf("run",0);
 	t_free * ok;
 	ok = ft_node_new(malloc(sizeof(1))); 
 	ft_malloc(3,ok);
 	ft_free_all(ok);
 	ft_free(NULL,ok);
-	char *str = (char *)ft_malloc(3 *sizeof(char),NULL);
-	str[0]='a';
-	int *test = ft_malloc(57345555 * sizeof(char), NULL );
-	  ft_malloc(57345555 * sizeof(char), NULL);
-	ft_malloc(57345555 * sizeof(char), NULL);
-	ft_malloc(57345555 * sizeof(char), NULL);
-
-	ft_free(test,NULL);
-	if(test == NULL)
-		printf("\nfinal\n");
-	//ft_free(str);
-
-	func_test();
-	char *teste = ft_malloc(90 * sizeof(char *), NULL);
-	ft_free_all(NULL);
 
 }
