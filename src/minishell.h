28b84bc2cd03648c8fe864d1f63cb0eb88bc4044
Rui Pires <rpires-c@c2r6s1.42porto.com>
2024-10-15 16:18:26 +0100

    added libraries for pipex

--- src/minishell.h
+++ src/minishell.h
@@ -6,7 +6,7 @@
 /*   By: rpires-c <rpires-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 12:46:57 by jperpect          #+#    #+#             */
-/*   Updated: 2024/10/15 15:18:56 by rpires-c         ###   ########.fr       */
+/*   Updated: 2024/10/15 16:17:42 by rpires-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,11 @@
 #include "./comands_line/readline.h"
 #include "./free/free.h"
 #include "../libft/libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+# include <sys/wait.h>
+# include <fcntl.h>
 
 #endif 
