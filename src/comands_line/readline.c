0d15703233ed5da4dcf73007f170d9223d741367
Joao Francisco Perpectuo Figueiredo <jperpect@c2r5s1.42porto.com>
2024-10-15 14:32:12 +0100

    read line start

--- src/comands_line/readline.c
+++ src/comands_line/readline.c
@@ -1,1 +1,17 @@
 #include "readline.h"
+
+
+
+void start_shell()
+{
+	char *line;
+	
+	while (1)
+	{
+		line = readline("hello how dead ?:");
+		if(line == NULL)
+			break;
+		add_history(line);
+		free(line);
+	}
+}
