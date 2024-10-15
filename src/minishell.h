f25856e7936b3b40e7c0acf82a6cd96bf2100ed9
Rui Pires <rpires-c@c2r6s1.42porto.com>
2024-10-15 15:20:48 +0100

    library struct overlap - not resolved - libft library removed

--- src/minishell.h
+++ src/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jperpect <jperpect@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rpires-c <rpires-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 12:46:57 by jperpect          #+#    #+#             */
-/*   Updated: 2024/09/10 13:13:05 by jperpect         ###   ########.fr       */
+/*   Updated: 2024/10/15 15:18:56 by rpires-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 
 #include "../libft/Get_next_line/get_next_line_bonus.h"
 #include "../libft/printf/ft_printf.h"
-#include "../libft/libft/libft.h"
 #include "./comands_line/readline.h"
+#include "./free/free.h"
+// #include "../libft/libft/libft.h"
 
 #endif 
