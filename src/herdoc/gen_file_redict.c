3c3a196f7ad20e1f634d838d3f0005bda48c584b
Joao Francisco Perpectuo Figueiredo <jperpect@c2r5s4.42porto.com>
2024-11-12 12:06:23 +0000

    is posibel change the fd in redicte

--- src/herdoc/gen_file_redict.c
+++ src/herdoc/gen_file_redict.c
@@ -6,14 +6,33 @@
 /*   By: jperpct <jperpect@student.42porto.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 10:27:15 by jperpct           #+#    #+#             */
-/*   Updated: 2024/11/12 10:32:02 by jperpct          ###   ########.fr       */
+/*   Updated: 2024/11/12 12:04:01 by jperpct          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+#include <unistd.h>
 
 void	change_file(t_minis *mini, int set)
 {
+	static int file_origin;
+	static int file_new[2];
+	if (set == 0)
+	{
+	 // delete file_new  create file_new clean copy fd=1 change fd =1 for fd = file_new
+		if (file_new > 0)
+			close(file_new);
+		file_origin = dup(1);
+		pipe(file_new);
+		dup2(file_new[0],1);
+
+	}
+	else if (set == 1)
+	{
+
+		dup2(file_origin,1);
+	// fd = file_origin excute the redicion expecific
+	}
 
 }
 
