f5363e2ca7920de6ec6b9e77478808403c40a408
Joao Francisco Perpectuo Figueiredo <jperpect@c1r1s3.42porto.com>
2024-11-26 17:35:10 +0000

    work the work

--- src/builtins/export/export.c
+++ src/builtins/export/export.c
@@ -6,7 +6,7 @@
 /*   By: jperpct <jperpect@student.42porto.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 15:05:55 by jperpct           #+#    #+#             */
-/*   Updated: 2024/11/15 15:22:14 by jperpct          ###   ########.fr       */
+/*   Updated: 2024/11/26 17:33:18 by jperpct          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,19 @@ void	ft_export_add( t_list_ *list, char *str, t_minis *mini)
 	char	**export;
 	char	*temp;
 	export = ft_split(str, '=');
-
+	
+	if (str[ft_strlen(export[0])-1] == '+')
+	{
+		temp = ft_substr(export[0], 0, ft_strlen(export[0])-1);
+		ft_free(export[0],NULL);
+		export[0] = temp;
+		temp = NULL;
+	}
 	if(ft_getenv_content(list, export[0]) != NULL){
 
 		env = ft_getenv_content(list, export[0]);
 		env->chek = TRUE;
-		if(export[1] != NULL)
+		if(export[1] != NULL && str[ft_strlen(export[0])-1] != '+')
 			ft_free(env->content,NULL);
 	}
 	else
@@ -37,13 +44,20 @@ void	ft_export_add( t_list_ *list, char *str, t_minis *mini)
 		env->content = NULL;
 	}
 	env->name = ft_strdup(export[0]);
-	if (export[1] != NULL )
+	if (export[1] != NULL  && str[ft_strlen(export[0])-1] != '+' )
 		env->content = ft_strjoin("",
 				&mini->split[1][strlen(export[0]) + 1]);
-	else if (str[ft_strlen(export[0])] == '=' )
+	else if (str[ft_strlen(export[0])] == '=' && str[ft_strlen(export[0])-1] != '+' )
 	{
 		temp = ft_malloc(1 * sizeof(char), NULL);
 		temp[0] = '\0';
+		env->content = temp;
+	}
+	else if (export[1] != NULL  && str[ft_strlen(export[0])-1] == '+')
+	{
+		temp = ft_strjoin(env->content,ft_strjoin("",
+				&mini->split[1][strlen(export[0]) + 1]));
+		ft_free(env->content,NULL);
 		env->content = temp;
 	}
 	
