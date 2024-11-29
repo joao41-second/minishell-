bf359a564900355645b0272b2f5c9c652f4a1acd
Joao Francisco Perpectuo Figueiredo <jperpect@c2r5s3.42porto.com>
2024-11-29 15:04:16 +0000

    exit leak in expand variabel

--- src/parsing/expand_env/expnad.c
+++ src/parsing/expand_env/expnad.c
@@ -6,44 +6,40 @@
 /*   By: jperpct <jperpect@student.42porto.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 15:59:23 by jperpct           #+#    #+#             */
-/*   Updated: 2024/11/15 16:15:47 by jperpct          ###   ########.fr       */
+/*   Updated: 2024/11/29 15:02:11 by jperpct          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-	
 #include "../../minishell.h"
 
-
-int ft_lenchar(char *str, char cha)
+int	ft_lenchar(char *str, char cha)
 {
-	int i;
-	
+	int	i;
+
 	i = -1;
-	while(str[++i] != '\0')
-		if(str[i] == cha)
-			break;
+	while (str[++i] != '\0')
+		if (str[i] == cha)
+			break ;
 	return (i);
 }
 
-char *concatenate_the_str_with_env_var(char *str,t_minis *mini, int *len)
+char	*concatenate_the_str_with_env_var(char *str, t_minis *mini, int *len)
 {
 	char	*ok;
 	char	*env;
-	int i;
-	
+	int		i;
+
 	(void)mini;
 	i = 0;
-	if(str[0] == '$')
+	if (str[0] == '$')
 	{
-		while(str[++i] != '\0')
+		while (str[++i] != '\0')
 		{
-			if(str[i] == ' ' || str[i] == '"' || str[i] == 39 || str[i] == '$')
+			if (str[i] == ' ' || str[i] == '"' || str[i] == 39 || str[i] == '$')
 				break ;
 		}
-		env = ft_substr(str,1, --i);
-		//if(ft_getenv(mini,env) == NULL)
-		//	return (NULL);
-		ok = ft_strjoin("", ft_getenv(mini,env));
-		*len = ft_strlen(env) +1;
+		env = ft_substr(str, 1, --i);
+		ok = ft_strjoin("", ft_getenv(mini, env));
+		*len = ft_strlen(env) + 1;
 		(void)len;
 		ft_free(env, NULL);
 		return (ok);
@@ -51,96 +47,86 @@ char *concatenate_the_str_with_env_var(char *str,t_minis *mini, int *len)
 	return ("");
 }
 
-int ft_if_expand(char *verifc, t_quotes *quotes, int i)
+int	ft_if_expand(char *verifc, t_quotes *quotes, int i)
 {
 	(void)i;
-	if((verifc[0] == '$' && quotes->dub == 1 ))
+	if ((verifc[0] == '$' && quotes->dub == 1))
 		return (TRUE);
-	if((verifc[0] == '$' && quotes->dub == 0 && quotes->simp == 0 ))
+	if ((verifc[0] == '$' && quotes->dub == 0 && quotes->simp == 0))
 		return (TRUE);
 	return (FALSE);
 }
 
 char	*ft_strjoin_and_free(char *s1, char *s2)
 {
-	char *save;
-	char *aloc;
+	char	*save;
+	char	*aloc;
+
 	save = ft_strdup(s1);
-	ft_free(s1,NULL);
+	ft_free(s1, NULL);
 	aloc = ft_strjoin(save, s2);
-	ft_free(save,NULL);
+	ft_free(save, NULL);
 	return (aloc);
 }
 
-
-char	*creat_new( int i, char *str, t_quotes *quotes,  t_minis *mini)
+char	*creat_new(int i, char *str, t_quotes *quotes, t_minis *mini)
 {
 	static char	*temp = NULL;
-	static int not_print;
-	char *save;
-	char verifc[2];
+	static int	not_print;
+	char		*save;
+	char		verifc[2];
 
 	verifc[0] = str[i];
 	verifc[1] = '\0';
-	if(not_print > 0)
-	{
+	if (not_print > 0)
 		not_print--;
-	}
-	if(verifc[0] == '\0')
+	if (verifc[0] == '\0')
 	{
 		not_print = 0;
-		ft_free(temp,NULL);
+		ft_free(temp, NULL);
 		temp = NULL;
 		return (NULL);
 	}
-
-	if(not_print == 0)
+	if (not_print == 0)
 	{
-			
-		if(temp == NULL)
+		if (temp == NULL)
 			temp = ft_strdup("");
-		
-		if(ft_if_expand(verifc,quotes,i) == TRUE)
+		if (ft_if_expand(verifc, quotes, i) == TRUE)
 		{
-			save = concatenate_the_str_with_env_var(&str[i],mini,&not_print);
-			if(save == NULL)
+			save = concatenate_the_str_with_env_var(&str[i], mini, &not_print);
+			if (save == NULL)
 			{
 				not_print = INT_MAX;
-				ft_free(temp,NULL);
+				ft_free(temp, NULL);
 				temp = NULL;
 				return (NULL);
 			}
 			temp = ft_strjoin(temp, save);
-			ft_free(save,NULL);
+			ft_free(save, NULL);
 			return (temp);
 		}
-
-		if(verifc[0] == '"' || verifc[0] == 39)
-		{ 
-			if(quotes->simp == 1 && quotes->dub == 1)
+		if (verifc[0] == '"' || verifc[0] == 39)
+		{
+			if (quotes->simp == 1 && quotes->dub == 1)
 			{
-				temp  = ft_strjoin_and_free(temp,verifc);
-				if(verifc[0] == '"' )
+				temp = ft_strjoin_and_free(temp, verifc);
+				if (verifc[0] == '"')
 					quotes->dub--;
-				if(verifc[0] == 39 )
+				if (verifc[0] == 39)
 					quotes->simp--;
-
 			}
-
 		}
 		if (verifc[0] != '"' && verifc[0] != 39)
-		{
-			temp  = ft_strjoin_and_free(temp,verifc);
-		}
+			temp = ft_strjoin_and_free(temp, verifc);
 	}
-		return (temp); 
+	return (temp);
 }
 
-char *expand_env(char *str, t_minis *mini)
+char	*expand_env(char *str, t_minis *mini)
 {
-	int i;
-	t_quotes quotes;
-	 char *end;
+	int			i;
+	t_quotes	quotes;
+	char		*end;
 
 	end = "";
 	(void)mini;
@@ -149,36 +135,32 @@ char *expand_env(char *str, t_minis *mini)
 	quotes.flags[1] = 0;
 	quotes.flags[2] = 0;
 	quotes.flags[3] = 0;
-	
-	end = creat_new(0, "\0", &quotes,mini);
+	end = creat_new(0, "\0", &quotes, mini);
 	while (str[++i] != '\0')
 	{
-		if(str[i] == 39)
-		{ 
+		if (str[i] == 39)
+		{
 			quotes.simp++;
 			quotes.simp_size = i;
 		}
-		if(str[i] == '"')
+		if (str[i] == '"')
 		{
 			quotes.dub++;
 			quotes.dub_size = i;
 		}
-
-		if(quotes.simp > 1)
+		if (quotes.simp > 1)
 		{
 			quotes.simp = 0;
 			quotes.simp_size = i;
 		}
-		if(quotes.dub > 1)
+		if (quotes.dub > 1)
 		{
 			quotes.dub = 0;
-			quotes.dub_size = i; 
+			quotes.dub_size = i;
 		}
-	//	ft_free(end,NULL);
-
-		end = creat_new(i, str, &quotes,mini);
-		if(end == NULL)
-			return (NULL); 
+		end = creat_new(i, str, &quotes, mini);
+		if (end == NULL)
+			return (NULL);
 	}
 	return (end);
 }
