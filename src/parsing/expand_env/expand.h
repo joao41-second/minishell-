e3040f042fafb44f3dbaf38be87cf1c8337ef711
Joao Francisco Perpectuo Figueiredo <jperpect@c2r5s3.42porto.com>
2024-11-29 15:45:46 +0000

    norminet in expand

--- src/parsing/expand_env/expand.h
+++ src/parsing/expand_env/expand.h
@@ -6,31 +6,53 @@
 /*   By: jperpct <jperpect@student.42porto.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 17:28:34 by jperpct           #+#    #+#             */
-/*   Updated: 2024/11/15 17:29:54 by jperpct          ###   ########.fr       */
+/*   Updated: 2024/11/29 15:45:10 by jperpct          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #define EXPAND_H
 #ifdef EXPAND_H
 
-typedef union s_quotes 
+typedef union s_quotes
 {
-	struct {
-	int simp;
-	int simp_size;
-	int dub;
-	int dub_size;
+	struct
+	{
+		int	simp;
+		int	simp_size;
+		int	dub;
+		int	dub_size;
 	};
-		int flags[4];
-	char *end;
+	int		flags[4];
+	char	*end;
 }			t_quotes;
 
+/**
+ * @brief the funcion expand env variabels in str
+ *
+ * @param str to env 
+ * @param mini struct the minishell 
+ * @return str env expand
+ */
+char		*expand_env(char *str, t_minis *mini);
 
-char *expand_env(char *str, t_minis *mini);
+/**
+ * @brief strjoin usenig the free in s2
+ *
+ * @param s1 sting
+ * @param s2 sring - is freed 
+ * @return new str new memory alocad
+ */
+char		*ft_strjoin_and_free(char *s1, char *s2);
 
-
-char	*ft_strjoin_and_free(char *s1, char *s2);
-
-char *concatenate_the_str_with_env_var(char *str,t_minis *mini, int *len);
+/**
+ * @brief concatenate str with env expand
+ *
+ * @param str to concatenate
+ * @param mini the struct the minishel 
+ * @param len int char $ the str 
+ * @return 
+ */
+char		*concatenate_the_str_with_env_var(char *str, t_minis *mini,
+				int *len);
 
 #endif // !EXPAND_H
