98df052e5b2dd9e368b1ca762235d7b4b96054cf
Joao Francisco Perpectuo Figueiredo <jperpect@c2r5s3.42porto.com>
2024-11-29 14:02:40 +0000

    free libery datasheet

--- libft/free/free_and_list.h
+++ libft/free/free_and_list.h
@@ -18,14 +18,38 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+/**
+ * @brief this function is used for allocate memory 
+ * @param size nuber bits for alocade 
+ * @param list_set set is null;
+ */
 void	*ft_malloc(size_t size, void *list_set);
 
+/**
+ * @brief this function is used to free memory of the program 
+ * @param list_set set is null
+ */
 void	ft_free_all(void *list_set);
 
+/**
+ * @brief this function is used for free in specifly variabel
+ * @param var for free memory
+ * @param list_set set ins null
+ */
 void	ft_free(void *var, void *list_set);
 
+/**
+ * @brief the start the library the malloc not use not 
+ * possible use neither the all function
+ */
 void	start_alloc(void);
 
+/**
+ * @brief alloc the memory reght for Created new node t_list_
+ *
+ * @param n variabel used in list
+ * @return node t_list_ alocad with memory alocade
+ */
 t_list_	*ft_node_new_free(void *n);
 
 #endif
