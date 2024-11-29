bb3542b1d70cf3e5c85a4dac31d5facef118861a
Joao Francisco Perpectuo Figueiredo <jperpect@c2r5s3.42porto.com>
2024-11-29 15:14:55 +0000

    not close the fd int redirecons

--- src/herdoc/ft_redict_comand_for_new_file/redirect_for_new_file.c
+++ src/herdoc/ft_redict_comand_for_new_file/redirect_for_new_file.c
@@ -6,48 +6,44 @@
 /*   By: jperpct <jperpect@student.42porto.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 12:30:26 by jperpct           #+#    #+#             */
-/*   Updated: 2024/11/12 12:54:52 by jperpct          ###   ########.fr       */
+/*   Updated: 2024/11/29 15:12:18 by jperpct          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-int redirect_for_new_file(t_minis *mini)
+int	redirect_for_new_file(t_minis *mini)
 {
-	int i;
-	int fds;
-	i = -1;
+	int	i;
+	int	fds;
 
+	i = -1;
 	while (mini->split[++i] != NULL)
 	{
-		if(ft_strncmp(mini->split[i], ">", 4) == 0)
+		if (ft_strncmp(mini->split[i], ">", 4) == 0)
 		{
 			i++;
-			break;
+			break ;
 		}
 	}
-	fds = open(mini->split[i],O_CREAT | O_WRONLY | O_TRUNC,0777);
-
+	fds = open(mini->split[i], O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	return (fds);
 }
 
-int redirect_for_add_file(t_minis *mini)
+int	redirect_for_add_file(t_minis *mini)
 {
-	int i;
-	int fds;
-	i = -1;
+	int	i;
+	int	fds;
 
+	i = -1;
 	while (mini->split[++i] != NULL)
 	{
-		if(ft_strncmp(mini->split[i], ">>", 4) == 0)
+		if (ft_strncmp(mini->split[i], ">>", 4) == 0)
 		{
 			i++;
-			break;
+			break ;
 		}
 	}
-	//unlink(mini->split[i]);	
-
-	fds = open(mini->split[i],O_CREAT | O_WRONLY | O_APPEND,0777);
-
+	fds = open(mini->split[i], O_CREAT | O_WRONLY | O_APPEND, 0644);
 	return (fds);
 }
