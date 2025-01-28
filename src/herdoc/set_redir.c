0c62745d4b9d0009c5cb6c4f4519f43bce19475f
Joao Francisco Perpectuo Figueiredo <jperpect@c2r6s2.42porto.com>
2025-01-28 10:09:23 +0000

    the errors

--- src/herdoc/set_redir.c
+++ src/herdoc/set_redir.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "../minishell.h"
+#include "errno.h"
 
 void	relink_list(t_list_ **list,void *content)
 {
@@ -54,27 +55,26 @@ void set_redir(t_list_ *list, t_btree **btree)
 void	set_fd(int nb,t_token *token,t_minis *mini)
 {
 	int fd=0;
-		
+	//char *redir;
+
 	if (nb == 1 && token->redirection_target != NULL)
 		fd = open(expand_env(token->redirection_target,mini), O_CREAT | O_WRONLY | O_APPEND, 0644);
 	if (nb == 2 && token->redirection_target != NULL)
 		fd = open( expand_env(token->redirection_target,mini),O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	if (nb == 3)
 		fd = herdoc(mini, 0,token->redirection_source);
-	if (nb == 4 && token->redirection_target != NULL)
-		fd = open(expand_env(token->redirection_source,mini), O_WRONLY);
+	if (nb == 4 && token->redirection_source != NULL)
+		fd = open(expand_env(token->redirection_source,mini), O_RDONLY );
 	
-	if(fd < 0 && (nb == 2 || nb ==1))
+	if(fd < 0 )
 	{
-		ft_print_error_simple("",NOT_PERM , "bash");
-		ft_exit_end(1);
-	}
-	if(fd < 0 && (nb == 4))
-	{
-		ft_print_error_simple("",NOT_FILE , "bash");
-		ft_exit_end(1);
-	}
+		if (errno == ENOENT) 
+			ft_print_error_simple("",NOT_FILE , "bash");
+		else
+			ft_print_error_simple("",NOT_PERM , "bash");
 
+		ft_exit_end(1);
+	}
 	if(nb == 2 || nb == 1)
 		dup2(fd,1);
 	if(nb == 3 || nb == 4)
